98204e75576580f4890f449eded6eca1cd2a33dc
Svanhildur12 <svanhildur12@gmail.com>
2024-04-11 19:12:53 +0000

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 "use client";
-import Image from "next/image";
-import { type } from "os";
-import { useState, Fragment, useCallback } from "react";
 import TeamClock from "./TeamClock";
 import TeamObject from "./TeamObject";
 import GameReviews from "./GameReviews";
+import ShoppingList from "./ShoppingList";
 
 export default function Home() {
   return (
     <>
       {/*  <TeamClock /> */}
       {/*  <TeamObject /> */}
-      <GameReviews />
+      {/*  <GameReviews /> */}
+      <ShoppingList />
     </>
   );
 }
