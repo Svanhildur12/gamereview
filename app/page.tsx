4b7e004b9f811548625c5186b6c46941700548e6
Svanhildur12 <svanhildur12@gmail.com>
2024-04-14 17:12:38 +0000

    re-done

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@
 import TeamClock from "./TeamClock";
 import TeamObject from "./TeamObject";
 import GameReviews from "./GameReviews";
+import ShoppingList from "./ShoppingList";
+import ExpensesDisplay from "./Expenses";
 
 export default function Home() {
   return (
     <>
       {/*  <TeamClock /> */}
       {/*  <TeamObject /> */}
-      <GameReviews />
+      {/* <GameReviews /> */}
+      {/* <ShoppingList/> */}
+      <ExpensesDisplay />
     </>
   );
 }
